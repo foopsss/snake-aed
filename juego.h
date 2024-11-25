95b8cc70cb814648d78c5d2ad71697d130ecce64
Lucas <libenchat@hotmail.com>
2024-11-25 00:18:23 -0300

    Versión casi final del juego

---
+++ juego.h
@@ -0,0 +1,382 @@
+//////////////////////
+///// Librerías. /////
+//////////////////////
+
+// Para poder usar printf y scanf.
+#include <stdio.h>
+
+// Para poder realizar llamadas a system.
+#include <stdlib.h>
+
+// Para funciones específicas de Windows, tales
+// como GetStdHandle(), SetConsoleCursorPosition(),
+// kbhit() y getch().
+#include <windows.h>
+#include <conio.h>
+
+///////////////////////
+///// Miscelánea. /////
+///////////////////////
+
+// Constantes para definir la velocidad del juego.
+#define FRAMERATE    64
+#define SPEED        150
+
+// Constantes correspondientes a los signos a utilizar a la hora de dibujar
+// el tablero, la serpiente y la manzana.
+#define ASTERISCO    42
+#define O_MAYUSCULA  79
+#define ESQ_SUP_IZQ  201
+#define ESQ_SUP_DER  187
+#define ESQ_INF_IZQ  200
+#define ESQ_INF_DER  188
+#define BARRA_VER    186
+#define BARRA_HOR    205
+
+// Constantes correspondientes a las teclas que el usuario puede presionar
+// durante el juego.
+#define ESCAPE       27
+#define ARRIBA       72
+#define IZQUIERDA    75
+#define DERECHA      77
+#define ABAJO        80
+
+// Formato de un registro de tipo "coordenadas".
+// Este formato lo vamos a utilizar para definir
+// un registro con las coordenadas de la manzana,
+// y otro con las coordenadas en las que se va a
+// ir moviendo la serpiente.
+struct coordenadas {
+    int x;
+    int y;
+};
+
+// Archivo de puntos, en el que vamos a almacenar
+// la puntuación de los jugadores.
+FILE* puntos;
+
+// Contadores para utilizar en varios ciclos "for".
+int i, j;
+
+// hConsole es una variable del tipo HANDLE provisto
+// en <windows.h>, que nos permite adueñarnos de la
+// ventana de la consola y modificar sus propiedades.
+HANDLE hConsole;
+
+void limpiar() {
+    system("cls");
+}
+
+void colorear_fondo() {
+    // Ponemos el color de fondo de la consola en negro.
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+}
+
+void trazar_linea(int longitud) {
+    for (i = 0; i < longitud - 1; i++) {
+        printf("-");
+    }
+    printf("-\n");
+}
+
+void presione_tecla() {
+    printf("Presione una tecla para continuar...");
+    getch();
+}
+
+void ocultar_cursor_consola(int flag) {
+    CONSOLE_CURSOR_INFO cursorInfo;
+    cursorInfo.dwSize = 100;
+
+    if (flag == 0) {
+        cursorInfo.bVisible = FALSE;
+    } else {
+        cursorInfo.bVisible = TRUE;
+    }
+
+    SetConsoleCursorInfo(hConsole, &cursorInfo);
+}
+
+void gotoxy(int x, int y) {
+    // Le restamos 1 a las coordenadas porque, de lo contrario, gotoxy nos
+    // deja una posición por delante de donde realmente esperamos estar
+    // cuando especificamos un par [X; Y] en los parámetros.
+    COORD c;
+    c.X = x - 1;
+    c.Y = y - 1;
+
+    SetConsoleCursorPosition(hConsole, c);
+}
+
+//////////////////////////////////////////
+///// Cosas relacionadas al tablero. /////
+//////////////////////////////////////////
+
+// Dimensiones del tablero.
+#define filas 25
+#define columnas 80
+
+// Posición inicial del tablero en pantalla.
+// Estas constantes definen a partir de dónde se empieza
+// a mostrar el tablero.
+#define pos_inicial_x 5
+#define pos_inicial_y 5
+
+// Estructura correspondientes al tablero.
+char tablero[filas][columnas];
+
+void cargar_tablero() {
+    for (i = 0; i < filas; i++) {
+        for (j = 0; j < columnas; j++) {
+            tablero[i][j] = ' ';
+        }
+    }
+}
+
+void mostrar_tablero(int pos_x, int pos_y) {
+    cargar_tablero();
+    gotoxy(pos_x, pos_y);
+
+    for (i = 0; i < filas + 2; i++) {
+        if (i == 0 || i == filas + 1) {
+            // Estamos trabajando en uno de los bordes
+            // del tablero.
+            if (i == 0) {
+                printf("%c", ESQ_SUP_IZQ);
+            } else {
+                printf("%c", ESQ_INF_IZQ);
+            }
+
+            for (j = 0; j < columnas; j++) {
+                printf("%c", BARRA_HOR);
+            }
+
+            if (i == 0) {
+                printf("%c", ESQ_SUP_DER);
+            } else {
+                printf("%c", ESQ_INF_DER);
+            }
+        } else {
+            // Estamos trabajando en uno de los puntos
+            // intermedios del tablero.
+            printf("%c", BARRA_VER);
+
+            for (j = 0; j < columnas; j++) {
+                // La matriz "tablero" está cargada
+                // desde cero, y si usamos el "i" como
+                // viene, nos estaríamos perdiendo una
+                // fila de la matriz por empezar a mostrar
+                // desde i = 1.
+                printf("%c", tablero[i - 1][j]);
+            }
+
+            printf("%c\n", BARRA_VER);
+        }
+
+        pos_y = pos_y + 1;
+        gotoxy(pos_x, pos_y);
+    }
+}
+
+//////////////////////////////////////////////
+///// Cosas relacionadas a la serpiente. /////
+//////////////////////////////////////////////
+
+// Formato de la lista que define a la serpiente.
+// Tipo de dato personalizado, de nombre "serpiente".
+typedef struct nodo {
+    int tipo; // 0 = cabeza, 1 = cuerpo
+    int x;
+    int y;
+    struct nodo* prox;
+} serpiente;
+
+// Tamaño de un nodo de tipo "serpiente".
+#define serp_size sizeof(serpiente)
+
+// Cabeza de la serpiente.
+serpiente* cabeza;
+
+// Variables a utilizar para manejar la serpiente.
+int pos_ant_x_cola;
+int pos_ant_y_cola;
+
+void crear_cabeza_serpiente() {
+    // Reservamos memoria para el primer elemento
+    // de nuestra serpiente, la cabeza.
+    cabeza = NULL;
+    cabeza = (serpiente*) malloc(serp_size);
+
+    // Le asignamos valores a los elementos de
+    // su nodo.
+    cabeza -> tipo = 0;
+    cabeza -> x = pos_inicial_x + columnas / 2;
+    cabeza -> y = pos_inicial_y + filas / 2;
+    cabeza -> prox = NULL;
+}
+
+void alargar_serpiente(serpiente *serp, int pos_x, int pos_y) {
+    // Debemos llegar hasta la cola de la serpiente
+    // para ubicar un nuevo elemento al final.
+    if (serp -> prox == NULL) {
+        serp -> prox = NULL;
+        serp -> prox = (serpiente*) malloc(serp_size);
+        serp -> prox -> tipo = 1;
+        serp -> prox -> x = pos_x;
+        serp -> prox -> y = pos_y;
+        serp -> prox -> prox = NULL;    
+    } else {
+        alargar_serpiente(serp -> prox, pos_x, pos_y);
+    }
+}
+
+void mostrar_serpiente(serpiente *serp) {
+    // En función del tipo del elemento, vamos a elegir un color
+    // para la parte de la serpiente.
+    if (serp -> tipo == 0) {
+        // Azul para la cabeza de la serpiente.
+        SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_BLUE);
+    } else if (serp -> tipo == 1) {
+        // Verde para el cuerpo.
+        SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_GREEN);
+    }
+
+    gotoxy(serp -> x, serp -> y);
+    printf("%c", ASTERISCO);
+
+    if (serp -> prox != NULL) {
+        mostrar_serpiente(serp -> prox);
+    } else {
+        // Con esto borramos los espacios en los que la serpiente
+        // ya no se encuentra tras haberse desplazado.
+        gotoxy(pos_ant_x_cola, pos_ant_y_cola);
+        printf(" ");
+    }
+}
+
+void mover_serpiente(serpiente *serp, int pos_x, int pos_y) {
+    // Con este método movemos la serpiente de atrás para adelante.
+    // Es decir, desde la cola hacia la cabeza. Esto significa que,
+    // por un breve momento, habrá dos elementos en la misma posición,
+    // aunque no es algo problemático porque el siguiente elemento
+    // se mueve de manera casi inmediata.
+    if (serp -> prox != NULL) {
+        // Estamos en la cabeza o en una posición intermedia, debemos
+        // avanzar hasta el último elemento.
+        //
+        // Nótese que en cada llamada, al próximo elemento le pasamos
+        // la posición del elemento actual, a la cual debe moverse el
+        // primero.
+        mover_serpiente(serp -> prox, serp -> x, serp -> y);
+    } else {
+        // Estamos en la cola, resguardamos su posición.
+        // Con las llamadas a la función que se encuentran apiladas, a
+        // medida que vayamos desapilándolas vamos a ir cambiando la
+        // posición de la cola y todos los elementos que le siguen.
+        pos_ant_x_cola = serp -> x;
+        pos_ant_y_cola = serp -> y;
+    }
+
+    serp -> x = pos_x;
+    serp -> y = pos_y;
+}
+
+int colision_tablero() {
+    int colision = 0;
+
+    // Detectamos la colisión con las filas y columnas del tablero.
+    // Lo hacemos de forma tal que, cuando la serpiente intente avanzar
+    // hacia la posición de una fila o columna, se corte el juego.
+    if (cabeza -> x == pos_inicial_x || cabeza -> x == pos_inicial_x + columnas + 1) {
+        colision += 1;
+    }
+
+    if (cabeza -> y == pos_inicial_y || cabeza -> y == pos_inicial_y + filas + 1) {
+        colision += 1;
+    }
+
+    return colision;
+}
+
+int colision_serpiente(serpiente *serp) {
+    if (serp != NULL) {
+        if (cabeza -> x == serp -> x && cabeza -> y == serp -> y) {
+            return 1;
+        } else if (serp -> prox != NULL) {
+            colision_serpiente(serp -> prox);
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
+}
+
+///////////////////////////////////////////
+///// Cosas relacionadas a la comida. /////
+///////////////////////////////////////////
+
+// Registro correspondiente a la manzana.
+struct coordenadas manzana;
+
+int colision_manzana(serpiente *serp) {
+    // Necesitamos que esta variable mantenga su
+    // valor a lo largo de las llamadas recursivas
+    // de un mismo bucle, así que la definimos como
+    // "static".
+    static int colision = 0;
+
+    if (serp == cabeza) {
+        // Estamos comenzando un nuevo bucle de
+        // comparaciones, así que limpiamos el
+        // valor de la variable colision.
+        colision = 0;
+    }
+
+    // Comparamos la posición ocupada por una parte de la serpiente
+    // con la posición cargada al azar para la manzana.
+    colision += (serp -> x == manzana.x) && (serp -> y == manzana.y);
+
+    if (colision == 0) {
+        // Si no hubo colisión con la parte tratada de la serpiente,
+        // comparamos con las otras partes.
+        if (serp -> prox != NULL) {
+            colision_manzana(serp -> prox);
+        }
+    }
+
+    // El "0" significa que no hubo una colisión con
+    // ninguna parte de la serpiente, mientras que
+    // un "1" indica lo opuesto.
+    return colision;
+}
+
+int random(int lim_sup) {
+    // Devuelve un número al azar, que no debe ser
+    // superior a una cota pasada como parámetro.
+    return rand() % lim_sup;
+}
+
+void crear_manzana() {
+    // Le asignamos coordenadas al azar a la manzana, de
+    // manera tal que aparezca en cualquier lugar dentro
+    // del tablero.
+    manzana.x = pos_inicial_x + random(columnas) + 1;
+    manzana.y = pos_inicial_y + random(filas) + 1;
+
+    if (colision_manzana(cabeza) != 0) {
+        // Si la posición asignada resulta ser una posición
+        // en la que se encuentra alguna parte de la serpiente,
+        // debemos repetir el proceso.
+        crear_manzana();
+    }
+}
+
+void mostrar_manzana() {
+    SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_RED);
+
+    gotoxy(manzana.x, manzana.y);
+    printf("%c", O_MAYUSCULA);
+
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+}
--- snake.c
+++ snake.c
@@ -1,32 +1,314 @@
-#include <stdio.h>
-#include "misc-func.h"
-#include "tablero.h"
+#include "juego.h"
+
+///// Variables globales a utilizar en el juego. /////
+
+// Puntaje del jugador.
+int puntaje = 0;
+
+// Longitud de la serpiente. Considera la cabeza de la
+// serpiente dentro del total.
+int longitud = 1;
+
+///// Funciones. /////
+
+void cargar_puntuacion() {
+    // String para almacenar el nombre del jugador luego
+    // de que termine la partida.
+    char nombre_jugador[100];
+
+    limpiar();
+
+    trazar_linea(110);
+    printf("Perdiste.\n");
+    printf("Recorda que no debes tocar los bordes del tablero ni hacer que la serpiente colisione consigo misma.\n");
+
+    trazar_linea(110);
+    printf("DATOS FINALES DE TU PARTIDA: \n");
+    printf("Puntaje: %d puntos\n", puntaje);
+    printf("Longitud de la serpiente: %d unidad/es\n", longitud);
+    printf("\n");
+    printf("Estos datos van a ser grabados en un archivo llamado 'puntuaciones.txt', el cual se va a encontrar en la\n");
+    printf("misma carpeta que el ejecutable de este juego.\n");
+    
+    trazar_linea(110);
+    printf("Antes de eso, por favor decinos como te llamas (100 caracteres como maximo).\n\n");
+    printf("Nombre: ");
+    scanf("%s", nombre_jugador);
+    puntos = fopen("puntuaciones.txt", "a");
+
+    if (puntos == NULL) {
+        // Debemos crear el archivo de puntuaciones.
+        puntos = fopen("puntuaciones.txt", "w");
+    }
+
+    if (longitud == 1) {
+        // NITPICK: si la serpiente solo tiene 1 unidad, usemos la escritura correcta.
+        fprintf(puntos, "Nombre: %s - Puntuacion: %d puntos - Longitud de la serpiente: %d unidad", nombre_jugador, puntaje, longitud);
+    } else if (longitud > 1) {
+        fprintf(puntos, "Nombre: %s - Puntuacion: %d puntos - Longitud de la serpiente: %d unidades", nombre_jugador, puntaje, longitud);
+    }
+
+    fprintf(puntos, "\n");
+    fclose(puntos);
+
+    trazar_linea(110);
+    printf("A continuacion, vas a ser devuelto/a al menu principal. Desde ahi podes volver a jugar, o revisar las\n");
+    printf("puntuaciones de partidas previas en la seccion 'Puntuaciones'.\n");
+    
+    trazar_linea(110);
+    presione_tecla();
+}
+
+void limpiar_lista(serpiente *nodo) {
+    serpiente* aux;
+
+    while (nodo != NULL) {
+        aux = nodo;
+        nodo = nodo -> prox;
+        free(aux);
+    }
+}
+
+void jugar() {
+    // Variables para controlar si el juego debe terminar o no, si
+    // si el usuario presionó una tecla, y para llevar la cuenta
+    // del tiempo que pasa hasta que se alcanza el límite de tiempo
+    // para realizar una acción con la serpiente.
+    int game_over = 0;
+    char tecla_presionada = 0;
+    int tiempo = 0;
+
+    // Variable para controlar si la serpiente está "digiriendo"
+    // una manzana o no.
+    int digestion = 0;
+
+    // Este registro nos va a indicar la dirección en la
+    // cual se debe mover la serpiente cuando el usuario
+    // presiona una tecla.
+    //
+    // Lo inicializamos de manera tal que la serpiente por
+    // defecto se mueva hacia arriba.
+    struct coordenadas direccion = {0, -1};
+
+    // Limpiamos la pantalla y ocultamos el cursor de la
+    // consola para que se vea un poco mejor el juego.
+    limpiar();
+    ocultar_cursor_consola(0);
+
+    // Mostramos el tablero, creamos la serpiente y la
+    // manzana, y los mostramos por pantalla.
+    mostrar_tablero(pos_inicial_x, pos_inicial_y);
+    crear_cabeza_serpiente();
+    crear_manzana();
+    mostrar_serpiente(cabeza);
+    mostrar_manzana();
+
+    while (game_over != 1) {
+        if (kbhit()) {
+            // Obtenemos la tecla presionada por el usuario.
+            tecla_presionada = getch();
+
+            // En función de la tecla presionada, determinamos la
+            // dirección de la serpiente.
+            switch(tecla_presionada) {
+            case ARRIBA:
+                direccion.x = 0;
+                direccion.y = -1;
+                break;
+            case ABAJO:
+                direccion.x = 0;
+                direccion.y = 1;
+                break;
+            case DERECHA:
+                direccion.x = 1;
+                direccion.y = 0;
+                break;
+            case IZQUIERDA:
+                direccion.x = -1;
+                direccion.y = 0;
+                break;
+            default:
+                break;
+            }
+        }
+
+        // Acumulamos los milisegundos que pasan hasta que se llegue
+        // al límite de tiempo para realizar un movimiento.
+        //
+        // Cuando se supera el límite, movemos la serpiente en la
+        // dirección establecida, sea la que eligió el usuario, o la
+        // dirección por defecto.
+        Sleep(FRAMERATE);
+        tiempo += FRAMERATE;
+
+        if (tiempo >= SPEED) {
+            mostrar_serpiente(cabeza);
+            mostrar_manzana();
+
+            // Mostramos el puntaje y la longitud alcanzada por la serpiente
+            // a medida que el jugador come manzanas.
+            gotoxy(pos_inicial_x, pos_inicial_y - 3);
+            printf("Puntaje: %d puntos", puntaje);
+            gotoxy(pos_inicial_x, pos_inicial_y - 2);
+            printf("Longitud de la serpiente: %d unidad/es", longitud);
+
+            mover_serpiente(cabeza, cabeza -> x + direccion.x, cabeza -> y + direccion.y);
+            game_over = colision_tablero() + colision_serpiente(cabeza -> prox) + (tecla_presionada == ESCAPE);
+
+            if (digestion == 0) {
+                // No hay una manzana en digestión, así que comprobamos
+                // si la serpiente comió otra manzana o no. Entrar en el
+                // siguiente bucle indicaría que chocamos con otra manzana.
+                if (colision_manzana(cabeza) != 0) {
+                    puntaje += 10;
+                    longitud += 1;
+                    digestion = 1;
+                }
+            } else {
+                // Una vez que se termine digerir la manzana, le añadimos
+                // otro elemento a la serpiente, y ponemos una nueva
+                // manzana en el tablero.
+                alargar_serpiente(cabeza, manzana.x, manzana.y);
+                crear_manzana();
+                digestion = 0;
+            }
+
+            tiempo = 0;
+        }
+    }
+
+    // Luego de una pequeña pausa, le pedimos al usuario que nos
+    // proporcione algo de información para poder registrar su
+    // puntaje.
+    //
+    // A partir de acá, ya no es necesario ocultar el cursor.
+    Sleep(5000);
+    ocultar_cursor_consola(1);
+    cargar_puntuacion();
+
+    // Reseteamos los contadores.
+    puntaje = 0;
+    longitud = 1;
+
+    // Una vez que el juego termine, limpiamos la memoria ocupada
+    // por los elementos de la serpiente.
+    limpiar_lista(cabeza);
+}
+
+void instrucciones() {
+    limpiar();
+    trazar_linea(110);
+    printf("Para jugar al Snake deben respetarse 2 reglas basicas: \n");
+    printf("\n");
+    printf("1. No deben tocarse los bordes del tablero.\n");
+    printf("2. La serpiente no debe colisionar consigo misma.\n");
+    printf("\n");
+    printf("Si haces cualquiera de estas dos cosas, la serpiente va a morir!\n");
+    printf("Trata de comer todas las manzanas que puedas, para que la serpiente se vuelva mas larga.\n");
+
+    trazar_linea(110);
+    printf("SIMBOLOS\n");
+    printf("--------\n");
+
+    // Cabeza de la serpiente.
+    printf("- ");
+    SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_BLUE);
+    printf("%c", ASTERISCO);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    printf(": ");
+    printf("caracter correspondiente a la cabeza de la serpiente.\n");
+
+    // Cuerpo de la serpiente.
+    printf("- ");
+    SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_GREEN);
+    printf("%c", ASTERISCO);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    printf(": ");
+    printf("caracter correspondiente a las unidades que conforman el cuerpo de la serpiente.\n");
+
+    // Manzana.
+    printf("- ");
+    SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_RED);
+    printf("%c", O_MAYUSCULA);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    printf(": ");
+    printf("caracter correspondiente a las manzanas que debe comer la serpiente.\n");
+
+    trazar_linea(110);
+    presione_tecla();
+}
+
+void puntuaciones() {
+    char ventana[200];
+
+    limpiar();
+    puntos = fopen("puntuaciones.txt", "r");
+    trazar_linea(110);
+
+    if (puntos == NULL) {
+        printf("No hay puntuaciones cargadas.\n");
+        printf("Luego de jugar algunas partidas vas a poder ver tus puntuaciones en el menu.\n");
+    } else {
+        while(fgets(ventana, 100, puntos) != NULL) {
+            printf("%s", ventana);
+        }
+        fclose(puntos);
+    }
+
+    trazar_linea(110);
+    presione_tecla();
+}
 
 int main() {
+    colorear_fondo();
+
+    // En Windows, para poder hacer ciertas acciones por pantalla necesitamos
+    // "adueñarnos" de la terminal (en caso de que trabajemos con CMD.exe).
+    //
+    // Para ello, cargamos el HANDLE que hemos definido en "juego.h", y que
+    // vamos a utilizar muchas veces a lo largo del programa.
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     char eleccion;
 
-    // Leemos un carácter porque entonces a la hora de compararlo no vamos
-    // a crashear todo el programa si un usuario introduce algo que no
-    // corresponde, o que no tiene el mismo tipo de dato, si trabajamos con
-    // un entero.
-    while (eleccion != '3') {
-        clrscr();
-        printf("Bienvenido al Snake!\n");
-        printf("Este es el menu principal, cuenta con las siguientes opciones: \n");
-        printf("--------------------------------------------------------------\n");
-        printf("1. Jugar.\n");
-        printf("2. Creditos.\n");
-        printf("3. Salir.\n");
+    while (eleccion != '4') {
+        limpiar();
+        ocultar_cursor_consola(0);
+        trazar_linea(80);
+        printf("Bienvenido/a al ");
+        SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_GREEN);
+        printf("Snake");
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+        printf("!\n");
+
+        trazar_linea(80);
+        printf("Esta es nuestra version del juego, producida por los siguientes estudiantes: \n");
+        printf("(*) Benchat Parra, Lucas Ivan\n");
+        printf("(*) Fedorchuk, Francisco Nicolas\n");
+        printf("(*) Romaniuk, Lucas Genaro\n");
+        printf("(*) Yaya, Franco Gabriel\n");
+
+        trazar_linea(80);
+        printf("Cuenta con las siguientes opciones: \n");
         printf("\n");
-        printf("Introduzca una opcion: ");
-        scanf(" %c", &eleccion);
+        printf("1. Jugar.\n");
+        printf("2. Instrucciones de juego.\n");
+        printf("3. Puntuaciones de partidas pasadas.\n");
+        printf("4. Salir.\n");
+        printf("\n");
+        printf("Presione el boton correspondiente a la opcion que desee elegir.");
+        eleccion = getch();
 
         switch(eleccion) {
         case '1':
+            jugar();
             break;
         case '2':
+            instrucciones();
             break;
         case '3':
+            puntuaciones();
+            break;
+        case '4':
             break;
         }
     }
